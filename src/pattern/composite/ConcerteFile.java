bd2d25a3e410cb2b645e26a3adf7b8d55329de6a
yanfeixiang <yanfeixiang@jd.com>
2016-11-17 19:33:07 +0800

    增加组合模式

---
+++ src/pattern/composite/ConcerteFile.java
@@ -0,0 +1,18 @@
+package pattern.composite;
+
+public class ConcerteFile extends File {
+    public ConcerteFile(String name) {
+        super(name);
+    }
+
+    @Override
+    public void add(File file) {
+        throw new RuntimeException("can not add file to file");
+    }
+
+    @Override
+    public void display(int depth) {
+        printPrefix(depth);
+        System.out.println(name);
+    }
+}
