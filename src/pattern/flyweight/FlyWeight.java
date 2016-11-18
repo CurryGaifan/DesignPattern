4371ed3367a89d4e459674a86b052e36b44dec74
yanfeixiang <yanfeixiang@jd.com>
2016-11-18 17:30:00 +0800

    添加flyweight模式

---
+++ src/pattern/flyweight/FlyWeight.java
@@ -0,0 +1,6 @@
+package pattern.flyweight;
+
+public interface FlyWeight {
+
+    void operation(String color);
+}
---
+++ src/pattern/flyweight/FlyWeightFactory.java
@@ -0,0 +1,20 @@
+package pattern.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlyWeightFactory {
+
+    private static Map<Character, FlyWeight> characterMap = new HashMap<>();
+
+
+    public static FlyWeight getFlyWeight(char c) {
+        FlyWeight flyWeight = characterMap.get(c);
+        if (flyWeight != null)
+            return flyWeight;
+
+        flyWeight = new ConcreteFlyWeight(c);
+        characterMap.put(c, flyWeight);
+        return flyWeight;
+    }
+}
